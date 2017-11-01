f6afb3f66b7f1ee9b72245b8e55f699abe616d6d
Fernando Lopez <fer.ragnar19@gmail.com>
2017-11-01 10:39:01 -0300

    crear mapa
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation, Geoposition } from "@ionic-native/geolocation";
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: any;
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation
+  ) {}
 
-  constructor(public navCtrl: NavController) {
+  ionViewDidLoad(){
+    this.getPosition();
+  }
 
+  getPosition():any {
+    this.geolocation.getCurrentPosition()
+    .then(response => {
+      this.loadMap(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  loadMap(position: Geoposition){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+
+    let mapEle: HTMLElement = document.getElementById('id');
+
+    let myLatLng = {lat: latitude, lng: longitude};
+
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Hola Gil'
+      });
+      mapEle.classList.add('show-map');
+    });
   }
 
 }
